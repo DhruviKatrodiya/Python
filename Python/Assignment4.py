6878ce9ff4475ceefc09362a199dccd35a251527
Dhruvi Katrodiya <103128211+DhruviKatrodiya@users.noreply.github.com>
2022-08-06 19:08:00 +0530

    Update Assignment4.py

--- Python/Assignment4.py
+++ Python/Assignment4.py
@@ -7,13 +7,14 @@ Created on Sat Aug  6 08:54:16 2022
 
 #Assignment Question 4
 def questionFive(num_list):
-firstElement = num_list[0] 
-lastElement = num_list[-1]
-if (firstElement == lastElement):
-return True
-else:
-return False
-
-xlistx = [10, 20, 30, 40, 190]
-
-print (questionFive(xlistx))
+    firstElement = num_list[0] 
+    lastElement = num_list[-1]
+    
+    if firstElement == lastElement:
+        return True
+    else:
+        return False
+    
+    xlistx = [10, 20, 30, 40, 190]
+    
+    print (questionFive(xlistx))
