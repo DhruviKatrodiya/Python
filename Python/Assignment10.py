93a9017092518a60cbb1c40e3216184fc84cb71f
Dhruvi Katrodiya <103128211+DhruviKatrodiya@users.noreply.github.com>
2022-08-10 08:27:07 +0530

    Update Assignment10.py

--- Python/Assignment10.py
+++ Python/Assignment10.py
@@ -6,8 +6,19 @@ Created on Sat Aug  6 09:16:55 2022
 """
 
 
-row=int(input("Enter the number"))
-for i in range (row):
+
+row=int(input("enter the number"))
+for i in range(row):
     for j in range(i+1):
         print(j+1, end=" ")
+    print("\r")
+          
         
+O/p:-
+    
+enter the number 5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
